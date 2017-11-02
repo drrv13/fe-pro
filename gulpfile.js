e85bb9037dde30dcdb14aa8c5415b1126f5664b8
drrv13 <omlevash@gmail.com>
2017-11-02 21:31:40 +0200

    added tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 ﻿'use strict'
 var gulp = require('gulp');
 var spritesmith = require('gulp.spritesmith');
-
+var pug = require('gulp-pug');
+var notify = require('gulp-notify');
+var sass = require('gulp-sass');
 gulp.task('hello',function(){
 	console.log('Hello!');
 });
@@ -22,3 +24,28 @@ gulp.task('sprite',function(){
 	var cssStream = spriteData.css.pipe(gulp.dest('src/scss/components/'));
 	return(imgStream, cssStream);
 });
+
+
+
+gulp.task('pug',function(){
+	return gulp.src(['src/pug/**/*.pug','!src/pug/**/_*.pug)'])
+		.pipe(pug({pretty:'\t'}))
+		.on("error", notify.onError())
+		.pipe(gulp.dest('app'));
+})
+
+
+
+
+gulp.task('sass',function(){
+	gulp.src('.src/scss/**/*.scss')
+	.pipe(sass(.on("error", notify.onError())))
+	.pipe(gulp.dest('app/css'))
+	
+});
+
+gulp.task('watch',function(){
+	gulp.watch('src/pug/**/*.pug',['pug']);
+	gulp.watch('src.scss/**/*.scss',['sass']);
+	gulp.watch('app/*.html',browserSync.reload);
+})
